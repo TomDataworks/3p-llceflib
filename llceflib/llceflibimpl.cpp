771b824e8b7f9952f41cb25d912c400ad57d4031
Drake Arconis <drake@alchemyviewer.org>
2016-09-17 13:58:54 -0400

    Remove hack

--- llceflib/llceflibimpl.cpp
+++ llceflib/llceflibimpl.cpp
@@ -224,9 +224,7 @@ void LLCEFLibImpl::update()
 
 void LLCEFLibImpl::shutdown()
 {
-#if !defined(__APPLE__)
     CefShutdown();
-#endif
 }
 
 void LLCEFLibImpl::setOnPageChangedCallback(std::function<void(unsigned char*, int, int, int, int, bool)> callback)
